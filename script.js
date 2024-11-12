c6016ac59e255e4282525896a194ae6cd5f75c51
Will <williesdescomunal@gmail.com>
2024-11-12 09:46:20 -0500

    Actualizacion

--- script.js
+++ script.js
@@ -1,54 +1,46 @@
-function validaDigito(value){
+function h12h24(value, hour) {
+  if(value === "12") {
+    return hour % 12 || 12;
+  }
+  return hour;
+}
+
+function getRadioChecked(value) {
+   for(let i=0; i<value.length; i++) {
+     if (value[i].checked) {
+         return value[i].value;
+     }
+   }
+   return "";
+}
+
+function getWeekNumber(date) {
+   const dt = new Date(date); // Copiamos la fecha para no modificar la original
+   dt.setHours(0, 0, 0, 0); // Establecemos el primer día de la semana como lunes (0)
+   dt.setDate(dt.getDate() + 4 - (dt.getDay() || 7));
+   return Math.ceil(((dt - new Date(dt.getFullYear(), 0, 1)) / 86400000 + 1) / 7);
+}
+
+let getValidateDigit = (value) => {
    return (value < 10) ? "0"+value : value;
 }
 
-function h12h24(value, hour){
-   if(value == "12"){
-      if(hour >= 12){
-         hour = hour - 12;
-      }
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-      if(hour == 0){
-         hour = 12;
-      } 
-   }
+function getData(dateNow) {
+   let dateAll = `${dateNow.getFullYear()} - ${getValidateDigit(dateNow.getMonth() + 1)} - ${getValidateDigit(dateNow.getDate())}`;
+   dateAll +=  "&nbsp;&nbsp;" + monthNames[dateNow.getMonth()] + "&nbsp;&nbsp; w:" + getWeekNumber(new Date(dateAll.replace(/\s+/g, "")));
 
-   if(value == "24"){
-      hour = hour;
-   }
-   return hour;
+   let radioButton = getRadioChecked(document.getElementsByName("h12-24"));
+
+   document.getElementById("day").textContent = dayNames[dateNow.getDay()];
+   document.getElementById("hours").textContent = getValidateDigit(h12h24(radioButton, dateNow.getHours())) + ":" + getValidateDigit(dateNow.getMinutes());
+   document.getElementById("seconds").textContent = getValidateDigit(dateNow.getSeconds());
+   document.getElementById("date").innerHTML = dateAll;
 }
 
-function radioChecked(value){
-   var h1224 = "";
-   for (var i=0; i<value.length; i++) {
-     if (value[i].checked == true) {
-         h1224 = value[i].value;
-         break;
-     }
-   }
-   return h1224;
-}
-
-function day(value){
-   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-   return days[value];
-}
-
-function mueveReloj(){ 
-   var dateNow = new Date();
-
-   var rc = radioChecked(document.getElementsByName("h12-24"));
-
-   document.getElementById("day").innerHTML = day(dateNow.getDay());
-   var hora = dateNow.getHours();
-   document.getElementById("hours").innerHTML = validaDigito(h12h24(rc, hora));;
-   document.getElementById("minutes").innerHTML = validaDigito(dateNow.getMinutes());
-   document.getElementById("seconds").innerHTML = validaDigito(dateNow.getSeconds());
-
-   document.getElementById("years").innerHTML = dateNow.getFullYear();
-   document.getElementById("months").innerHTML = validaDigito(dateNow.getMonth()+1);
-   document.getElementById("days").innerHTML = validaDigito(dateNow.getDate());
-
+function mueveReloj() {
+   getData(new Date());
    setTimeout("mueveReloj()", 1000);
 }
